fa4070627b62d12ba40a2c7efb507722cd8cd78c
Khanh <knives142@gmail.com>
2025-09-30 16:55:39 +0700

    Add files via upload

---
+++ Week 1/deep.py
@@ -0,0 +1,5 @@
+answer = input("what is the answer to the Great Question of Life, the Universe and Everything? ").lower()
+if answer == "fourty two" or answer == "fourty-two" or int(answer) == 42:
+    print("True")
+else:
+    print("False")
